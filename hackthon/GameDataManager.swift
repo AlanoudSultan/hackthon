8b50db6b1e24a0661fcc3387288a87f380854d1c
AlanoudSultan <abaalkhailalanoud@gmail.com>
2025-07-31 12:39:59 +0300

    stages done

--- hackthon/GameDataManager.swift
+++ hackthon/GameDataManager.swift
@@ -33,9 +33,10 @@ class GameDataManager: ObservableObject {
               let stagesData = try? JSONDecoder().decode(FinanceStagesData.self, from: data) else {
             // Fallback to hardcoded stages if JSON loading fails
             stages = [
-                FinanceStage(name: "مبتدئ", averageDailyIncome: 50, saving: 10, investment: 5, goal: "ادخر حتى تصل إلى 100 قطعة لتطوير الكشك الأول."),
-                FinanceStage(name: "متوسط", averageDailyIncome: 150, saving: 30, investment: 30, goal: "قم بتوسيع الكشك وإضافة منتجات (500 قطعة)."),
-                FinanceStage(name: "متقدم", averageDailyIncome: 300, saving: 60, investment: 90, goal: "امتلك متجرًا كبيرًا في السوق (1000 قطعة).")
+                FinanceStage(name: "مبتدئ", averageDailyIncome: 50, saving: 10, investment: 5, goal:  "ادخر حتى تصل إلى ١٠٠ قرش لتطوير الكشك الأول."),
+                FinanceStage(name: "متوسط", averageDailyIncome: 150, saving: 30, investment: 30, goal:  "ادخر حتى تصل الى ٥٠٠ قرش وقم بتوسيع الكشك وإضافة منتجات ."),
+                FinanceStage(name: "متقدم", averageDailyIncome: 300, saving: 60, investment: 90, goal:  "ادخر حتى تصل الى ١٠٠٠ قرش وامتلك متجرًا كبيرًا في السوق  ."),
+                FinanceStage(name: "تاجر" , averageDailyIncome: 600, saving: 120, investment: 240, goal: "ادخر حتى تصل الى ٢٠٠٠ قرش وامتلك متجرًا اخر في السوق")
             ]
             return
         }
@@ -94,6 +95,9 @@ class GameDataManager: ObservableObject {
             return currentMoney >= 500
         case 2: // متقدم
             return currentMoney >= 1000
+        case 3:
+            return currentMoney >= 2000
+            
         default:
             return false
         }
