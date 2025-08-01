2a3e6e637f1a0f96e7a68c172025a369fa22f337
HessaMoha <hessaalajaleen@gmail.com>
2025-08-01 15:55:38 +0300

    تعدي ،و اضافة صفحة محل اللاعب

---
+++ hackthon/userStore.swift
@@ -0,0 +1,18 @@
+//
+//  userStore.swift
+//  hackthon
+//
+//  Created by حصه العجالين on 07/02/1447 AH.
+//
+
+import SwiftUI
+
+struct userStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    userStore()
+}
